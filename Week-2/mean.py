1c26087b825dee583c54451897487140667b92a1
IsaacThairu <zackthyru@gmail.com>
2023-02-19 20:53:38 +0300

    lesson

--- Week-2/mean.py
+++ Week-2/mean.py
@@ -3,5 +3,5 @@ sum_num= 0
 for number in numbers:
     sum=sum_num + number
     avg_num=sum/10
-    print("the mean of the numbers is,avg_num")
+    print("the mean of the numbers is",avg_num)
     
--- Week-2/numbers.py
+++ Week-2/numbers.py
@@ -1,6 +1,14 @@
-numbers=[]
+#write a program to list all odd numbers from 1-100
+for odd_numbers in range(1,100):
+    if (odd_numbers%2 !=0):
+        print(odd_numbers)
+        
+#write a program to list all prime numbers from 1-100
+for prime_numbers in range(1,100):
+    if prime_numbers >1:
+        for i in range (1,100):
+            if (prime_numbers% i) == i:
+                print(prime_numbers)
 
-for number in numbers:
-    number=int (input("enter the number"))
-    sum_num = sum_num + number
-    
+
+        
